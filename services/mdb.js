a1f5398975779e463e26bc8a026af06690793033
MakenzieRoberts <makenzie.roberts@keyin.com>
2022-12-02 17:17:59 -0330

    Semicolon

--- services/mdb.js
+++ services/mdb.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 
 const uri = process.env.ATLAS_URI;
-const pool = new MongoClient(uri)
+const pool = new MongoClient(uri);
 
 module.exports = pool;
